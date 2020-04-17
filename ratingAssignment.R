4a54241f1e40caeccce12050676f618c58c8697a
Paul Novak <pnovak@alumni.caltech.edu>
2020-04-17 08:33:45 -0700

    Reformat

--- ratingAssignment.R
+++ ratingAssignment.R
@@ -2,20 +2,18 @@
 
 # required library: tidyverse
 
-# required input: 
+# required input:
 # items: a vector of items that need to be rated
 # raters: a vector of raters' names or IDs
 # pair_size: the number of raters assigned for a given item
 
-# output: 
-# The output is a list of 3 data frame: 
-# goal_record shows the names of raters assigned to each goal 
+# output:
+# The output is a list of 3 data frame:
+# goal_record shows the names of raters assigned to each goal
 # rater_record shows all the goals assigned to each rater
 # sum_record shows the total number of goals being assigned, and the total number of goals assigned to each rater
 
-ratingAssignment <- function(items, raters, pair_size){
-  
-  
+ratingAssignment <- function(items, raters, pair_size) {
   # extract the total number of raters
   raNum <- length(raters)
   
@@ -32,10 +30,10 @@ ratingAssignment <- function(items, raters, pair_size){
   pairNum <- nrow(raPairs)
   
   # randomize the order of the ra pairs
-  raPairs <- raPairs[sample(pairNum),]
+  raPairs <- raPairs[sample(pairNum), ]
   
   # get rid of empty items
-  items <- items[items!="" & is.na(items) == F]
+  items <- items[items != "" & is.na(items) == F]
   
   # randomize items
   items <- as.vector(sample(items))
@@ -46,7 +44,7 @@ ratingAssignment <- function(items, raters, pair_size){
   # modify the length of the items so that it can be divided by the number of pairs. The additional items will be NAs.
   length(items) <- prod(dim(matrix(items, nrow = pairNum)))
   
-  # assign items across all pairs and transform the result into a dataframe 
+  # assign items across all pairs and transform the result into a dataframe
   itemMatrix <- matrix(items, nrow = pairNum)
   itemMatrix <- as.data.frame(itemMatrix)
   
@@ -54,25 +52,27 @@ ratingAssignment <- function(items, raters, pair_size){
   assignDf <- cbind(raPairs, itemMatrix)
   
   # transform the dataframe into a long format
-  assignDf <- assignDf %>% 
+  assignDf <- assignDf %>%
     gather(key = order, value = items, starts_with("V")) %>%
     select(-order) %>%
     filter(!is.na(items))
   
   # calculate the maximum number of items assigned for each RA
-  rowMax <- length(items) * (pair_size/raNum)
+  rowMax <- length(items) * (pair_size / raNum)
   
   # initialize an empty dataframe where each rater is represented by a column
   raDf <- data.frame(matrix(nrow = rowMax, ncol = raNum))
   colnames(raDf) <- raters
   
-  # loop through each rater and extract the goals assigned to the rater pairs that this rater belongs 
-  for (raIdx in 1:length(raters)){
+  # loop through each rater and extract the goals assigned to the rater pairs that this rater belongs
+  for (raIdx in 1:length(raters)) {
     ra <- raters[raIdx]
-    pairIdx <- which(raPairs == ra, arr.ind = T)[,1] # see which pair includes this rater
-    item <- itemMatrix[pairIdx,] # extract the goals from these pairs
-    raItems <- as.vector(as.matrix(item)) # vectorize the goals 
-    raDf[,raIdx] <- sample(raItems) # randomize the order
+    pairIdx <-
+      which(raPairs == ra, arr.ind = T)[, 1] # see which pair includes this rater
+    item <-
+      itemMatrix[pairIdx, ] # extract the goals from these pairs
+    raItems <- as.vector(as.matrix(item)) # vectorize the goals
+    raDf[, raIdx] <- sample(raItems) # randomize the order
   }
   
   # get a summary of the number of items assigned to each rater
@@ -83,7 +83,7 @@ ratingAssignment <- function(items, raters, pair_size){
                     total_items = total_items)
   log <- cbind(log, bind_rows(assign_sum))
   
-  # generate a list to store all output dataframes 
+  # generate a list to store all output dataframes
   out <- list()
   
   out$goal_record <- assignDf
